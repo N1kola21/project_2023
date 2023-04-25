b7c526d0707085142015291128e9551b0a3cf045
N1kola21 <94890258+N1kola21@users.noreply.github.com>
2023-04-25 19:43:57 +0200

    Delete rocnikovka_druhak_version2 directory

--- rocnikovka_druhak_version2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rocnikovka_druhak_version2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.rocnikovka_druhak_version2 to javafx.fxml;
-    exports com.example.rocnikovka_druhak_version2;
-}
